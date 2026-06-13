5b7e8bd08fca329400b99b9a7eb86c65250b378c
Rahul11-debug <Rahul.kumarundergrade@gmail.com>
2026-06-13 14:26:02 +0530

    Update CORS origin to production URL

--- virtual-ui-server/index.js
+++ virtual-ui-server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://ui-library-b0wu.onrender.com", 
     credentials: true,               
   }) 
 );
